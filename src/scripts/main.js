3b157bc7f030b216dca4d04086cbf12a1aa8c4e9
Vitalii Venher <vengervn@gmail.com>
2025-03-11 00:57:59 +0200

    add slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 10,
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    }
+  }
+});

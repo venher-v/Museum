18b10bf55668a47b67519b987bd9cbccb57993dd
Vitalii Venher <vengervn@gmail.com>
2025-03-18 00:32:00 +0200

    fixed slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,51 @@
 'use strict';
 
-const swiper = new Swiper('.swiper', {
-  loop: true,
-  slidesPerView: 1,
-  spaceBetween: 10,
+let swiper = null;
+const galeryList = document.querySelector('.galery__list');
 
-  pagination: {
-    el: '.swiper-pagination',
-  },
+function initSwiper() {
+  galeryList.classList.add('swiper-wrapper');
 
+  swiper = new Swiper('.swiper', {
+    loop: true,
+    slidesPerView: 1.07,
+    spaceBetween: 16,
 
-  breakpoints: {
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 20
-    }
+    breakpoints: {
+      768: {
+        slidesPerView: 2.07,
+        spaceBetween: 20,
+      },
+    },
+
+    pagination: {
+      el: '.swiper-pagination',
+      bulletClass: 'my-bullet',
+      bulletActiveClass: 'my-bullet-active',
+    },
+  });
+
+  console.log('Swiper initialized');
+}
+
+function destroySwiper() {
+  if (swiper) {
+    swiper.destroy();
+    galeryList.classList.remove('swiper-wrapper');
+    swiper = null;
   }
-});
+
+  console.log('Swiper destroyed');
+}
+
+function windowResizeHandler() {
+  if (window.innerWidth < 1280 && !swiper) {
+    initSwiper();
+  } else if (window.innerWidth >= 1280 && swiper) {
+    destroySwiper();
+  }
+}
+
+windowResizeHandler();
+
+window.addEventListener('resize', windowResizeHandler);
